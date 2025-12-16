00b5e405dc8b7b355bae9aaddbab1353cf3ed397
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2025-12-16 10:47:26 -0500

    Basic Note Component
    Co-Authored-By: rurre <16716633+rurre@users.noreply.github.com>

---
+++ Runtime/ModularAvatarNote.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace nadena.dev.modular_avatar.core
+{
+	[AddComponentMenu("Modular Avatar/MA Note")]
+	[HelpURL("https://modular-avatar.nadena.dev/docs/reference/note?lang=auto")]
+	public class ModularAvatarNote : AvatarTagComponent
+	{
+		[SerializeField][TextArea(5,20)] internal string m_note = "";
+		[PublicAPI]
+		public string Note
+		{
+			get => m_note;
+			set => m_note = value;
+		}
+	}
+}
